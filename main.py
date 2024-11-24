6dcf94a93337f9dd5b25f94a69adbf5fccace86d
Berk-Can-Ulubas <berkcan.ulubas@gmx.de>
2024-11-24 14:25:24 +0100

    fenster erstellt mir roten Rechteck welches sich bewegen lässt

---
+++ main.py
@@ -0,0 +1,28 @@
+import pygame
+
+pygame.init()
+
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+player = pygame.Rect(300, 350, 50, 50)
+
+while True:
+    pygame.draw.rect(screen, (255, 0, 0), player)
+    key = pygame.key.get_pressed()
+    if key[pygame.K_a]:
+        player.move_ip(-1, 0)
+    elif key[pygame.K_d]:
+        player.move_ip(1, 0)
+    elif key[pygame.K_w]:
+        player.move_ip(0, -1)
+    elif key[pygame.K_s]:
+        player.move_ip(0, 1)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    pygame.display.update()
